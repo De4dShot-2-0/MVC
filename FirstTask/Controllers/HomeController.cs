b8d3a0ac6a96df6bca13648fbb4b431d18b4f5e2
Vlad Feitser <feysaball123@gmail.com>
2020-02-11 23:45:23 +0200

    FirstTask

---
+++ FirstTask/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstTask.Models;
+
+namespace FirstTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var articles = new Article().CreateTestList();
+
+            return View(articles);
+        }
+        public ActionResult Guest(string Name, string Text)
+        {
+            if (Request.HttpMethod == "GET")
+            {
+                return View(Comments.comments);
+            }
+            else
+            {
+                Comments.comments.Add(new Article(Name, Text));
+                return View(Comments.comments);
+            }
+        }
+        public ActionResult Blank()
+        {
+            return View();
+        }
+        public ActionResult Result(FormCollection answers)
+        {
+            int score = 0;
+            if (answers["Radio"] == "One")
+                score++;
+            if (answers["Text"].ToUpper() == "BALL")
+                score++;
+            if (Convert.ToBoolean(answers["Check1"].Split(',')[0]) == true &&
+                Convert.ToBoolean(answers["Check2"].Split(',')[0]) == true &&
+                Convert.ToBoolean(answers["Check3"].Split(',')[0]) == false)
+                score++;
+            return View(score);
+        }  
+    }
+}
---
+++ FirstTask/Models/Article.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstTask.Models
+{
+    public class Article
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+        public Article(string Name, string Text)
+        {
+            this.Name = Name;
+            this.Text = Text;
+            Date = DateTime.Now;
+        }
+        public Article ()
+        {
+
+        }
+        public List<Article> CreateTestList()
+        {
+            List<Article> testList = new List<Article>();
+            testList.Add(new Article("Name1", "TextTextTextTextText"));
+            testList.Add(new Article("Name2", "TextTextTextTextText"));
+            testList.Add(new Article("Name3", "TextTextTextTextText"));
+            testList.Add(new Article("Name4", "TextTextTextTextText"));
+            return testList;
+        }
+    }
+}
